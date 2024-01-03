50a7e07a8388c216986c9fe367ef613583f4ce2d
lk4eiji <charmander_16sep@hotmail.com>
2024-01-03 16:29:57 -0600

    Update: Agregando errores básicos. Primer versión funcional

--- pokebot.py
+++ pokebot.py
@@ -5,42 +5,55 @@ class Pokemon:
     def __init__(self,name):
         self.name = name
         self.api_data = pb.pokemon(name)
+        try:
+            self.get_id()
+        except AttributeError as e:
+            self.api_data = None
+
+    def get_id(self):
+        """
+        It gets the pokemon's ID, wich I will use it to check 
+        if the queried pokemon exist. 
+        If we attempt to search for a non-existent Pokémon using PokeBase, the library 
+        connected to PokeAPI, it will return an 'AttributeError', With that value, 
+        I can use a 'try' statement. This way, we prevent the application 
+        from closing as a result of that error.
+        Returns:
+            int: An integer with the Pokemon's ID
+        """
+        id = self.api_data.id
+        return id
 
     def get_info(self):
         """
-        Obtene la información del pokemon
+        It gets the detailed information about a queried pokemon
         Returns:
-            str: información del pokemon
+            str: A formatted string containing information about the Pokémon 
+            including its name, attack, defense, special attack
+            special defense, speed, and weight.
         """
         attack = self.api_data.stats[1].base_stat
-        deffense = self.api_data.stats[2].base_stat
+        defense = self.api_data.stats[2].base_stat
         specialAttack = self.api_data.stats[3].base_stat
-        specialDeffense = self.api_data.stats[4].base_stat
+        specialDefense = self.api_data.stats[4].base_stat
         speed = self.api_data.stats[5].base_stat
-        weight = self.api_data
+        weight = self.api_data.weight
             
-        
         return f'Estadisticas base:\n' \
-               f'Nombre: {self.name}\nAtaque: {attack}\nDefensa:{deffense}' \
-               f'Ataque Especial: {specialAttack}\nDefensa Especial: {specialDeffense}' \
+               f'Nombre: {self.name}\nAtaque: {attack}\nDefensa: {defense}\n' \
+               f'Ataque Especial: {specialAttack}\nDefensa Especial: {specialDefense}\n' \
                f'Velocidad: {speed}\nPeso: {weight}'
     
-#Get pokemon's ID
-def get_pokemonId(pokemonName):
-    if pokemonName:
-        pokemon = pb.pokemon(pokemonName)
-        pokeId = pokemon.id
-        return pokeId
-    else:
-        return 'Pokemon no encontrado'
-
-#Get Pokemon's Sprite
-def get_pokemonImg(pokemonId):
-    if pokemonId:
-        pokeImg = pb.SpriteResource('pokemon',pokemonId)
-        return pokeImg.url
-    else:
-        return 'Pokemon no encontrado'
+    def get_img(self):
+        """
+        it gets the queried pokemon's Sprite
+        Returns:
+            str: A string containing the URL of the Pokemon's sprite.
+        """
+        id = self.api_data.id
+        img = pb.SpriteResource('pokemon',id)
+        
+        return img.url
 
 #Connectivity with the bot
 TOKEN = '6889333455:AAGBmOjViTSK4Rx4GY443eb5NWuwesGujTQ' #YOUR_TOKEN_HERE
@@ -60,11 +73,14 @@ def send_help(message):
 def send_pokemon(message):
     pokeName = message.text.split()[1] if len(message.text.split()) > 1 else None
     if pokeName:
-        pokeId = get_pokemonId(pokeName)
-        pokeInfo = get_pokemon(pokeName)
-        pokeImg = get_pokemonImg(pokeId)
-        bot.send_photo(chat_id=message.chat.id, photo=pokeImg)
-        bot.reply_to(message,pokeInfo)
+        queriedPokemon = Pokemon(pokeName)
+        if queriedPokemon.api_data != None:
+            pokeInfo = queriedPokemon.get_info()
+            pokeImg = queriedPokemon.get_img()
+            bot.send_photo(chat_id=message.chat.id, photo=pokeImg)
+            bot.reply_to(message,pokeInfo)
+        else:
+            bot.reply_to(message,"Pokemon no encontrado")
     else:
         bot.reply_to(message,"Por favor proporciona el nombre del pokemon, por ejemplo: /search charmander")
 
