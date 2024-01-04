34a3f8070d7af408e43e70f8eecc25ba0db9e409
lk4eiji <charmander_16sep@hotmail.com>
2024-01-03 18:39:20 -0600

    Update: cambiando formato de mensaje de busqueda y actualizando datos

--- pokebot.py
+++ pokebot.py
@@ -44,10 +44,10 @@ class Pokemon:
         speed = self.api_data.stats[5].base_stat
             
         return f'Nombre del Pokemon: {self.name}\n' \
-               f'Número de Pokedex: {self.id}' \
-               f'Estadisticas base:\n' \
-               f'Ataque: {attack}\nDefensa: {defense}\n' \
-               f'Ataque Especial: {specialAttack}\nDefensa Especial: {specialDefense}\n' \
+               f'Número de Pokedex: {self.id}\n' \
+               f'\nESTADISTICAS BASE\n' \
+               f'Ataque: {attack}\tDefensa: {defense}\n' \
+               f'Ataque Especial: {specialAttack}\tDefensa Especial: {specialDefense}\n' \
                f'Velocidad: {speed}\n'
     
     def get_img(self):
